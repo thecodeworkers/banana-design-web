2b54ac7f4b294e2f25eeb06a4077453bbc59308d
Rodolfo Mayora <rodolfomayora96@gmail.com>
2020-09-21 17:10:45 -0400

    navbar: add Toggle Button PNG

--- src/assets/img/index.ts
+++ src/assets/img/index.ts
@@ -1,1 +1,3 @@
-export const logo = require('./Static/banana-logo.png');
+export const
+  logo = require('./Static/banana-logo.png'),
+  toggleButton = require('./Static/toggle-button.png');
--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import { logo } from './../../assets/img';
+import { logo, toggleButton } from './../../assets/img';
 import './styles.scss';
 
 const NavBar: React.FC<{}> = () => {
   return (
     <div className="_navBar">
       <img className="_navBarLogo" src={ logo } alt="Logo"/>
+
+      <img className="_navBarToggle" src={ toggleButton } />
     </div>
   )
 }
