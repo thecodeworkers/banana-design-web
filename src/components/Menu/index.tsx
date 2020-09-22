a4b11808bc3cff8db2ad2f393a072877a8355787
MauriceMiot <maurice.yggdrasil@gmail.com>
2020-09-22 15:00:58 -0400

    initial commit

---
+++ src/components/Menu/index.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import './style.scss';
+
+const Menu = () => (
+  <div className={'_menuContainer'}>
+    <div className={'_sectionWhite'}>
+      <p>blanco</p>
+    </div>
+    <div className={'_sectionBlack'}>
+      <div className={'blackHeader'}>
+      <div className={'_address'}>
+      <p className={'_addressText'}>Centro de Artes Integradas,</p>
+      <p className={'_addressText'}>Montaña Creativa</p>
+      <p className={'_addressText'}>Módulo 5 / Oficina 53</p>
+      <p className={'_addressText'}>Caracas 1073</p>
+      </div>
+      </div>
+  </div>
+  </div>
+)
+
+export default Menu
+
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import {Menu} from "../components"
 
 const IndexPage = () => (
-  <div>
-    <h1>Banana</h1>
-  </div>
+
+  <Menu />
+
 )
 
 export default IndexPage
