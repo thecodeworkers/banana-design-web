a8f78ac0c0172afc4c60804210f79c60c5bf9861
MauriceMiot <maurice.yggdrasil@gmail.com>
2020-09-23 09:23:30 -0400

    more text

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -21,18 +21,35 @@ const Menu = () => (
       </div>
 
       <div className={'_blackBody'}>
-      <p className={'_blackBodyText'}>Estudio</p>
-      <p className={'_blackBodyText'}>Creativos</p>
-      <p className={'_blackBodyText'}>Folio</p>
+        <p className={'_blackBodyText'}>Estudio</p>
+        <p className={'_blackBodyText'}>Creativos</p>
+        <p className={'_blackBodyText'}>Folio</p>
       </div>
       <div className={'_blackBodyFooter'}>
-      <p className={'_blackFooterText'}>Proyectos Destacados</p>
-      <p className={'_blackFooterText'}>Marcas Asociadas</p>
-      <p className={'_blackFooterText'}>Classroom</p>
+        <p className={'_blackBodyFooterText'}>Proyectos Destacados</p>
+        <p className={'_blackBodyFooterText'}>Marcas Asociadas</p>
+        <p className={'_blackBodyFooterText'}>Classroom</p>
       </div>
-      
+
       <div className={'_blackFooter'}>
         <div className='_blackFooterLine'></div>
+        <div className={'_blackFooterContainer'}>
+          <div >
+            <p className={'_blackFooterText'}>Contacto</p>
+            <p className={'_blackFooterText'}>contact@bananadsg.com</p>
+            <p className={'_blackFooterText'}>+58 412 222 2222</p>
+          </div>
+          <div >
+            <p className={'_blackFooterText'}>Buscas trabajar con nosotros?</p>
+            <p className={'_blackFooterText'}>Escríbenos a:</p>
+            <p className={'_blackFooterText'}>work@bananadsg.com</p>
+          </div >
+          <div>
+            <p className={'_blackFooterText '}>No te pierdas de nada,</p>
+            <p className={'_blackFooterText'}>síguenos en:</p>
+            <p className={'_blackFooterText'}>@bananadesign_</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
