25f311e03ec9f3a516604eb31de40ee1d060d87f
Rodolfo Mayora <rodolfomayora96@gmail.com>
2020-09-22 21:35:59 -0400

    navbar: add function to change colors

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,12 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Logo, ToggleButton } from './../../assets/img';
 import './styles.scss';
 
 const NavBar: React.FC<{}> = () => {
-  return (
-    <div className="_navBar">
-      <Logo width={"160"}/>
+   
+  const [isDark, setIsDark] = useState( false );
 
+  const changeColors = () => {
+
+    if ( window.scrollY > window.innerHeight * 0.85 ) {
+      setIsDark( true );
+    } else {
+      setIsDark( false );
+    }
+  }
+
+  useEffect(() => {
+
+    window.addEventListener( 'scroll', changeColors );
+
+    return () => {
+      window.removeEventListener( 'scroll', changeColors );
+    } 
+  }, [])
+
+  return (
+    <div className={`_navBar ${ isDark ? '_dark' : '_light'}`}>
+      <Logo className="_navBarLogo" width={"160"}/>
       <ToggleButton className="_navBarToggle" />
     </div>
   )
