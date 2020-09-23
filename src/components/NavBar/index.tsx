39b151be52914ed4bdf8765dc66b2636f5f41851
Rodolfo Mayora <rodolfomayora96@gmail.com>
2020-09-22 23:08:09 -0400

    navbar: define chage colors point

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,23 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { Logo, ToggleButton } from './../../assets/img';
+import navBarProps from './interface';
 import './styles.scss';
 
-const NavBar: React.FC<{}> = () => {
+
+const NavBar: React.FC<navBarProps> = ({ reference }) => {
    
   const [isDark, setIsDark] = useState( false );
 
+  const defaultChangeColorsPoint = window.innerHeight * 0.7;
+    
+  
   const changeColors = () => {
 
-    if ( window.scrollY > window.innerHeight * 0.85 ) {
+    const changeColorsPoint = reference 
+      ? reference.current.clientHeight
+      : defaultChangeColorsPoint;
+
+    if ( window.scrollY > changeColorsPoint ) {
       setIsDark( true );
     } else {
       setIsDark( false );
