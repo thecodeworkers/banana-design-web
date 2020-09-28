5177e4f22836af735e4bf601a81e4f2f4ab1564c
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-09-28 12:12:57 -0400

    no navbar

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,10 @@ import { Link } from "gatsby";
 import {Menu , Product} from "../components";
 
 const IndexPage = () => (
-<div>
-<Product />
-<Menu />
- 
-</div>
-
+	<div>
+		{/* <Product /> */}
+		<Menu />
+	</div>
 )
 
 export default IndexPage
