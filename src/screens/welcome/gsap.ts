f3c13816cb03c0ee7dd0aa1e30f2fe73ffc61da2
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-09-30 14:04:26 -0400

    animations

--- src/screens/welcome/gsap.ts
+++ src/screens/welcome/gsap.ts
@@ -2,9 +2,9 @@ import { gsap } from 'gsap/all';
 
 export const gsapStart = () => {
   gsap.timeline()
-    .from("._description, ._target, ._targetRightContainer", {opacity: 0, ease: "back", duration: 2.5})
+    .from("._description, ._target, ._targetRightContainer, ._contact", {opacity: 0, ease: "back", duration: 2.5})
     .from("#one", {opacity: 0, y: 10, duration: 0.2}, "-=1")
     .from("#two", {opacity: 0, y: 10, duration: 0.2}, ">")
     .from("#three", {opacity: 0, y: 10, duration: 0.2}, ">")
-    .to("._separator", {scale: 0, duration: 0.8, height: 6}, "<");
+    .from("._separator", {scale: 0, duration: 0.8, height: 6}, "<");
 }
