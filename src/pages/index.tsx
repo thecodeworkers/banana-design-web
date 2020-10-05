145a54ac833a44389b4ea728c5655b5d42dc4ed0
Rodolfo Mayora <rodolfomayora96@gmail.com>
2020-10-05 17:22:26 -0400

    general: remove test content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,8 @@ import { Featured, FeaturedTwo } from '../screens';
 const IndexPage = () => (
 	<div>
 		<NavBar />
-
 		<Menu />
 		<VerticalBreadcrumb />
-    <div style={{height: "100vh", backgroundColor: "#ffffff"}} ></div>
-    <div style={{height: "100vh", backgroundColor: "#444444"}} ></div>
-    <div style={{height: "100vh", backgroundColor: "#ffffff"}} ></div>
 	</div>
 )
 
