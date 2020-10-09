6d8d2ee5a810667fb89488a3927f8d71218a0874
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-10-09 14:51:11 -0400

    animations

--- src/screens/welcome/index.tsx
+++ src/screens/welcome/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import './styles.scss';
-import { gsapStart, gsapRetract } from './gsap';
+import { gsapStart, gsapRetract, gsapExpand } from './gsap';
 import { connect } from 'react-redux';
 
 const Welcome = (props) => {
@@ -14,7 +14,10 @@ const Welcome = (props) => {
 	}, [loader]);
 
 	useEffect(() => {
+		console.log(menu.opened);
+
 		if(menu.opened) gsapRetract();
+		if(menu.opened == false) gsapExpand();
   }, [menu]);
 
   return (
@@ -25,6 +28,14 @@ const Welcome = (props) => {
           <div className='_whiteBodyText' id='two'>Belleza + Función</div>
           <div className='_whiteBodyText' id='three'>Creación + Innovación</div>
         </section>
+				<ul className='_list'>
+					<li className='_smallBodyText' id='text-1'>+Concepto</li>
+					<li className='_smallBodyText' id='text-2'>+Propósito</li>
+					<li className='_smallBodyText' id='text-3'>+Belleza</li>
+					<li className='_smallBodyText' id='text-4'>+Función</li>
+					<li className='_smallBodyText' id='text-5'>+Creación</li>
+					<li className='_smallBodyText' id='text-6'>+Innovación</li>
+				</ul>
         <section className='_intermediate'>
           <div className='_target'>BRANDING, UI - UX, ANIMACIÓN & 3D, SOCIAL MEDIA</div>
           <div className='_targetRightContainer'>
@@ -34,16 +45,12 @@ const Welcome = (props) => {
           <div className='_separator'></div>
         </section>
         <section className='_contact'>
-					<ul className='_list'>
-						<li className='_smallBodyText' id='text-1'>+Concepto</li>
-						<li className='_smallBodyText' id='text-2'>+Propósito</li>
-						<li className='_smallBodyText' id='text-3'>+Belleza</li>
-						<li className='_smallBodyText' id='text-4'>+Función</li>
-						<li className='_smallBodyText' id='text-5'>+Creación</li>
-						<li className='_smallBodyText' id='text-6'>+Innovación</li>
-					</ul>
           <div className='_contactText' style={{marginRight: '25%'}}>contact@bananadsg.com</div>
           <div className='_contactText'>+58 412 222 2222</div>
+					<p className='_moreInfo'>
+						Somos un estudio creativo enfocado en ofrecer soluciones de diseño a nivel de Branding, Diseño Web,
+						Modelado 3D y Social Media. Trabajamos con innovadores que buscan mejorar el mundo a través de sus ideas y servicios.
+					</p>
         </section>
         <section className='_description'>
           <div style={{padding: '20% 0 10% 15%'}}>
