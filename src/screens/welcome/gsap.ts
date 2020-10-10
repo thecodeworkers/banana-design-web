2a92bcb46e537c1d7798353d85a1e4a65fd5c8ef
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-10-10 14:27:45 -0400

    arrow animation

--- src/screens/welcome/gsap.ts
+++ src/screens/welcome/gsap.ts
@@ -2,12 +2,12 @@ import { gsap } from 'gsap/all';
 
 export const gsapStart = () => {
   gsap.timeline()
-		.from("._description, ._target, ._targetRightContainer, ._contact", { opacity: 0, ease: "back", duration: 2.5 })
+		.from("._description, ._target, ._targetRightContainer, ._contact, ._arrow", { opacity: 0, ease: "back", duration: 2.5 })
 		.from("#one", { opacity: 0, y: 10, duration: 0.2 }, "-=1")
 		.from("#two", { opacity: 0, y: 10, duration: 0.2 }, ">")
 		.from("#three", { opacity: 0, y: 10, duration: 0.2 }, ">")
 		.from("._separator", { scale: 0, duration: 0.8, height: 1.5 }, "<")
-		.from("._arrow", { y: 100, duration: 0.5 });
+		.from("._arrow", { opacity: 0, y: -150}, "-=1");
 }
 
 export const gsapRetract = () => {
