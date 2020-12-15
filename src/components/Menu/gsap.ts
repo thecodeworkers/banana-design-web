d7dacb11a6a3135fe64e93b0fb221d2e7e075b9a
Liseth Gutierrez <liseth.yggdrasil@gmail.com>
2020-12-15 17:09:54 -0400

    alineacion

--- src/components/Menu/gsap.ts
+++ src/components/Menu/gsap.ts
@@ -8,9 +8,9 @@ export const gsapMenuStart = () => {
     .play()
 		.to("._sectionBlack", 1,{ x: '0%'}, 0.4)
 		.to("._breadCrumbtextOne", 0.5, { x: '0%', opacity: 1 }, 2.4)
-    .to("._blackFooterLine", 1.5, { width: '100%', }, 1.2)
-    .to("._blackFooterHover", 1.5, { width: '0%', }, 1.2)
-    .to("._blackHeaderHover", 1.5, { width: '0%', }, 1.2)
+    .to("._blackFooterLine", 1.5, { width: '85%' }, 1.2)
+    .to("._blackFooterHover", 1.5, { width: '0%' }, 1.2)
+    .to("._blackHeaderHover", 1.5, { width: '0%' }, 1.2)
     .to("._blackBodyFooterHover", 0.6, { width: '0%'}, 1.2)
 		.to("._blackBody", 0.1, { opacity:1, }, 2.5)
 		.eventCallback("onComplete", () => console.log("abierto"));
--- src/screens/welcome/gsap.ts
+++ src/screens/welcome/gsap.ts
@@ -15,15 +15,18 @@ export const gsapRetract = () => {
 	.to("#two", { opacity: 0, y: 10, duration: 0.1 })
 	.to("#three", { opacity: 0, y: 10, duration: 0.1 })
 	.to("._description, ._targetRightContainer, ._contactText, ._arrow, ._languageButton", { opacity: 0, duration: 0.2 })
-	.to("._intermediate", { duration: 0.4, y: -90 })
-	.to("._intermediate", { duration: 0.5, y: 173 })
+	.to("._intermediate", { duration: 0.4, "--bottomPosition" : "20%"})
+	// .to("._intermediate", { duration: 0.4, y: -90 })
+	// .to("._intermediate", { duration: 0.5, y: 173 })
+	// .to("._intermediate", { duration: 0.5 })
 	.to("._smallBodyText", { opacity: 1, stagger: { each: 0.05 } }, "-=0.2" )
 	.to("._moreInfo", { opacity: 1, ease: "back", duration: 0.5 }, ">");
 }
 
 export const gsapExpand = (toggle) => {
 	gsap.timeline().delay(1.5)
-	.to("._intermediate", { duration: 1, y: 0 }, ">")
+	// .to("._intermediate", { duration: 1, y: 0 }, ">")
+	.to("._intermediate", { duration: 1, "--bottomPosition" : "50%"}, ">")
 	.to("._moreInfo", { opacity: 0, ease: "back", duration: 1 }, "-=1")
 	.to("._smallBodyText", { opacity: 0, duration: 0.05, stagger: { each: 0.05, from: 6 } }, "-=1")
 	.to("._separator", { width: '100%', duration: 1.5, height: 1.5 })
